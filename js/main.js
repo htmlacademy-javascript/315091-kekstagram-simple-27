6ca241c05745f7877c119caa3cf3a9ac984299e0
Olia <oliaermakk@gmail.com>
2023-01-02 14:51:27 +0300

    добавила функции для создания массива из постов

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-function getRandomNumber (firstNumber, secondNumber) {
+//варианты комментариев для поста
+const descriptionOptions = [
+  'сегодня отличный день', 'в кексограме очень весело', 'позанимался 2 часа', 'включила новый плейлист',
+  'кошечка заснула прямо на руках', 'сегодня много заданий', 'прочитала новую книгу'];
+
+//данные которые даны в задании - в константы
+
+//количество постов в массиве
+const SIMILAR_PHOTO_POSTS_COUNT = 5;
+
+//количество лайков
+const LIKES_COUNT = {
+  min: 15,
+  max: 200,
+};
+
+//количество комментариев
+const COMMENTS_COUNT = {
+  min: 0,
+  max: 200,
+};
+
+//рандомное положительное число в переданном диапазоне
+const getRandomNumber = (firstNumber, secondNumber) => {
   if (firstNumber < 0 || secondNumber < 0) {
     return NaN;
   }
@@ -8,13 +31,39 @@ function getRandomNumber (firstNumber, secondNumber) {
   const randomNumber = Math.random() * (maxNumber - minNumber + 1) + minNumber;
 
   return Math.floor(randomNumber);
-}
+};
 
-getRandomNumber(3, 12);
+//рандомный элемент по индексу из массива
+const getRandomArrayElement = (someArray) => {
+  return someArray[getRandomNumber(0, someArray.length - 1)];
+};
 
-function checkCommentLength (string, length) {
+const createPhotoPost = (index) => {
+  return {
+    id: index,
+    url: `photos/${index}.jpg`,
+    description: getRandomArrayElement(descriptionOptions),
+    likes: getRandomNumber(LIKES_COUNT.min, LIKES_COUNT.max),
+    comments: getRandomNumber(COMMENTS_COUNT.min, COMMENTS_COUNT.max),
+  };
+};
+
+//финальный массив из постов
+const getPhotoPosts = Array.from({length:  SIMILAR_PHOTO_POSTS_COUNT}, createPhotoPost);
+
+/*
+const getIndex = () => {
+  return  Array.from({length: getPhotoPosts.length}, (_, photoPostIndex) => createPhotoPost(photoPostIndex + 1));
+}; */
+
+const getIndex = () => {
+  return getPhotoPosts.map((_, photoPostIndex) => createPhotoPost(photoPostIndex + 1));
+};
+
+function checkStringLength (string, length) {
   return string.length <= length;
-}
+};
 
-checkCommentLength('', 140);
-
+console.log(getIndex());
+console.log(getPhotoPosts);
+checkStringLength('', 140);
